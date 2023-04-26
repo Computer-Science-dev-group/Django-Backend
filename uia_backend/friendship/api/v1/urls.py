f4b124c285e67dfdc05a91b4f0c76ded410a9088
JoseMiracle <101978290+JoseMiracle@users.noreply.github.com>
2023-04-26 14:12:22 +0100

    Update urls.py

--- uia_backend/friendship/api/v1/urls.py
+++ uia_backend/friendship/api/v1/urls.py
@@ -7,7 +7,7 @@ from friendship.api.v1.views import(
 )
 
 urlpatterns = [
-        path("send-friend-request/<str:receiver_id>/", SendFriendRequestView.as_view(), name="send-friend-request"),
+        path("send-friend-request/<str:sender_id>/<str:receiver_id>/", SendFriendRequestView.as_view(), name="send-friend-request"),
         path("accept-friend-request/<int:pk>/", AcceptFriendRequestView.as_view(), name="accept-friend-request"),
         path("reject-friend-request/<int:pk>/", RejectFriendRequestView.as_view(), name="reject-friend-request"),
         path("block-friend/<int:pk>", BlockFriendView.as_view(), name="block-friend"), 
