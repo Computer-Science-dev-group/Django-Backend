00b37ae9ede26458b985fcaa4927007fea78d77d
JoseMiracle <josephmiracle119@gmail.com>
2023-09-06 06:52:23 +0100

    Fixed JSONDecodeError and added 1min/req throttle to change password api

--- uia_backend/accounts/api/v1/throttles.py
+++ uia_backend/accounts/api/v1/throttles.py
@@ -1,5 +1,10 @@
-from rest_framework.throttling import AnonRateThrottle
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 
 
 class PasswordRestThrottle(AnonRateThrottle):
     THROTTLE_RATES = {"anon": "1/minute"}
+
+
+class ChangePassswordThrottle(UserRateThrottle):
+    scope = "sustained"
+    rate = "1/minute"
